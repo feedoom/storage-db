f5c9d3a37c9d40892477c3b7b3cba523550eb1b0
weyos <m18770047842@163.com>
2019-01-06 19:07:12 +0800

    精简版

--- README.md
+++ README.md
@@ -1,12 +1,28 @@
 # storage-db
 
-精简版indexDB，简易移动端使用，完善中。。。
+精简版indexDB，简易移动端使用。
 
 ## 安装
 
 ```sh
 npm install dbstorage
 ```
+
+## 引入
+
+```js
+import StorageDB from 'dbstorage';
+```
+
+默认不做任何编译处理
+
+如需编译处理过的js
+
+```js
+import StorageDB from 'dbstorage/lib/storage';
+```
+
+Promise需自行引入polyfill
 
 ## 初始化
 
--- rollup.config.js
+++ rollup.config.js
@@ -2,20 +2,36 @@ import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import regenerator from 'rollup-plugin-regenerator';
 // import async from 'rollup-plugin-async';
+import commonjs from 'rollup-plugin-commonjs';
+import { uglify } from 'rollup-plugin-uglify';
 
-export default {
-  input: 'src/storageDB.js',
-  output: {
-    file: 'dist/storage.js',
-    format: 'umd',
-    name: 'StorageDB',
+const plugins = [
+  resolve(),
+  uglify(),
+  commonjs(),
+  babel({
+    exclude: 'node_modules/**',
+  }),
+  regenerator(),
+];
+
+export default [
+  {
+    input: 'src/storageDB.js',
+    output: {
+      file: 'lib/storage.js',
+      format: 'umd',
+      name: 'StorageDB',
+    },
+    plugins,
   },
-  plugins: [
-    resolve(),
-    // async(),
-    babel({
-      exclude: 'node_modules/**',
-    }),
-    regenerator(),
-  ],
-};
+  // {
+  //   input: 'src/core/storage.js',
+  //   output: {
+  //     file: 'lib/storage-little.js',
+  //     format: 'umd',
+  //     name: 'StorageDB',
+  //   },
+  //   plugins,
+  // }
+];
